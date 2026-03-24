fde4808de3fe2ace044c1061fcb2d49bab5b6b7f
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2026-03-24 15:13:41 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -15,5 +15,17 @@ fi
 
 # Auto-reload current zsh session
 source "$ZSHRC"
+
+# Auto-reload only if in zsh
+if [[ -n ${ZSH_VERSION:-} ]]; then
+  source "$ZSHRC"
+  echo "✓ Reloaded ~/.zshrc - MY_VAR available!"
+else
+  echo "✓ Added to ~/.zshrc. Reload manually: source ~/.zshrc"
+fi
+
+exec zsh
+
+# Running MY_VAR
 echo "✓ Reloaded ~/.zshrc - MY_VAR is now available"
 echo "Test: echo \$MY_VAR"
